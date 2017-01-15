7b5f8817607cca7f9ac734b0e9d94a8d2f5ab768
GwangMok Ko <chrcra@naver.com>
2017-01-16 00:17:10 +0900

    Create Soution Common folder
    ...

--- KCL/K/stdafx.h
+++ KCL/K/stdafx.h
@@ -10,6 +10,6 @@
 #include <stdio.h>
 #include <tchar.h>
 
-
-
-// TODO: reference additional headers your program requires here
+#include "../Common/CommonHeader.h"
+#include "../Common/CommonFuction.h"
+#include "../Common/CommonDef.h"
