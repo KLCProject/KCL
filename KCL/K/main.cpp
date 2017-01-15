dbaa55e392bc31db1c40a6c853651da5f9c53863
GwangMok Ko <chrcra@naver.com>
2017-01-16 01:33:42 +0900

    add boost
    ...

--- KCL/K/main.cpp
+++ KCL/K/main.cpp
@@ -1,6 +1,15 @@
 #include "stdafx.h"
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
 
 int main()
 {
+	// test
+	using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
+
+	std::for_each(in(std::cin), in(), std::cout << (_1 * 3) << " ");
     return 0;
 }
